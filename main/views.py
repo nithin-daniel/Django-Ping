8dd0003aec463e3c115bd1092854a70ef1e9476d
nithindaniel1 <nithindaniel2018@gmail.com>
2022-10-19 19:50:39 +0530

    issues fixed

--- main/views.py
+++ main/views.py
@@ -2,22 +2,44 @@ from rest_framework.views import APIView
 from .models import url
 from .serializers import urlSerializer
 from rest_framework import viewsets
-from django.http import HttpResponse
 import os 
+from rest_framework.response import Response
+from django.http import HttpResponse
+from rest_framework import status
+import urllib.request
+from urllib.request import urlopen
+from urllib.error import *
 
 
 class urllistapi(viewsets.ModelViewSet):
     serializer_class = urlSerializer
     def create(self,request,*args, **kwargs):
         
-        url = request.data.get('url')
-        response = os.system("ping -c 1 " + url)
+        url = request.data['url']
+        print(url)
+        # response = os.system("ping -c 1 " + url)
+        # print(response)
 
-            #and then check the response...
-        if response == 0:
-            return HttpResponse('Website is on live')
-            #  print(url, 'is up!')
+
+        # try block to read URL
+        try:
+            html = urlopen(url)
+            
+        # except block to catch
+        # exception
+        # and identify error
+
+            
+        except URLError as e:
+            print("Opps ! Page not found!", e)
+            return HttpResponse("Opps ! Page not found!")
+        
         else:
-            return HttpResponse('Website is not in live')
-            #  print(url, 'is down!')
+            print('Yeah !  found ')
+            return HttpResponse('Yeah !  found ')
+
+
+
+
+
 
